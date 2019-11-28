9021ef8b52db99c93240614b5dcee2513a6100c6
Mateko <mateuszpierzchala01@gmail.com>
2019-11-28 20:40:52 +0100

    Updates

---
+++ precache-manifest.83f9eda78acc6fd83d29983bf1c2082e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34a1d5e7d917c2091fc9b159cd69610f",
+    "url": "/Weather-App/index.html"
+  },
+  {
+    "revision": "62f9e033d9ea73e01e16",
+    "url": "/Weather-App/static/css/main.78443967.chunk.css"
+  },
+  {
+    "revision": "9e9ea7ae2ad53bdda61c",
+    "url": "/Weather-App/static/js/2.2ae2b743.chunk.js"
+  },
+  {
+    "revision": "62f9e033d9ea73e01e16",
+    "url": "/Weather-App/static/js/main.509d0719.chunk.js"
+  },
+  {
+    "revision": "e6c3feae64726e096ecc",
+    "url": "/Weather-App/static/js/runtime-main.35a429e0.js"
+  }
+]);
